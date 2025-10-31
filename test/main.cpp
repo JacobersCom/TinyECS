d84d871ce9bfafe026aa5eb4adea128503220448
JacobersCom <jacobymartin1@icloud.com>
2025-10-30 23:24:23 -0400

    Build setting set

---
+++ test/main.cpp
@@ -0,0 +1,8 @@
+#include <tinyecs/entity.hpp>
+#include <iostream>
+
+int main() {
+	entity en;
+	std::cout << "Hello World!";
+	return 0;
+}
---
+++ tinyecs/include/tinyecs/entity.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+class entity{
+
+    public:
+    
+    int object;
+
+};
